8330a5962927ba6eee45e50b95c23a85e20dd354
f3rran <brechyt@gmail.com>
2021-08-25 20:24:35 +0200

    Avanzada la sección de componentes

--- componentes/lib/src/pages/home_temp.dart
+++ componentes/lib/src/pages/home_temp.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({Key? key}) : super(key: key);
+
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis'];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItemsCorta(),
+      ),
     );
+  }
+
+  List<Widget> _crearItems(){
+
+    List<Widget> lista = [];
+
+    for(String opt in opciones){
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)
+           ..add(Divider());
+    }
+
+    
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta(){
+
+    return opciones.map((e) {
+        
+        return Column(
+          children: [
+            ListTile(
+              title: Text(e),
+              subtitle: Text('Subtitulo'),
+              leading: Icon(Icons.ad_units_sharp),
+              trailing: Icon(Icons.keyboard_arrow_right),
+            ),
+            Divider(),
+          ],
+        );
+    }).toList();
+
   }
 }
